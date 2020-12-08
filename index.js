1ae15083168a9bcba6e566c64142481a652b8e15
g3ntl3m3n <somethingsmeels@gmail.com>
2020-12-08 22:46:18 +0300

    google strategy options

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth').OAuthStrategy;
+const keys = require('./config/keys.js');
+
 const app = express();
 
-app.get('/', (req, res) =>
-{
-    res.send({hi : 'there'});
-});
+//create new instance with googlestrategy
+//console.developers.google.com
+passport.use(new GoogleStrategy({
+    ClientID: keys.googleClientID,
+    ClientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+}, accessToken => {
+    console.log(accessToken)
+})
+);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
